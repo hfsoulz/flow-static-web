1505d155683e1881fef89e7748c137660f665703
Andreas Widen <aw@luflow.net>
2025-12-05 18:21:54 +0100

    hide header when scrolling down and show when scrolling up.
    Signed-off-by: Andreas Widen <aw@luflow.net>

--- static/scripts/core.js
+++ static/scripts/core.js
@@ -7,3 +7,38 @@ let header = document.querySelector('header');
 menuToggle.onclick = function () {
     header.classList.toggle('active');
 };
+
+// hide header navigation bar when scrolling down, and show again when scrolling up:
+let lastScrollTop = 0;
+let navbarHeight = 100;
+let didScroll = false;
+let delta = 5;
+
+window.onscroll = function () {
+    didScroll = true;
+};
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let scrollTop = document.documentElement.scrollTop;
+
+    if (Math.abs(lastScrollTop - scrollTop) <= delta) {
+        return;
+    }
+
+    if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
+        header.classList.add('nav-up');
+        header.classList.remove('nav-down');
+    } else {
+        header.classList.remove('nav-up');
+        header.classList.add('nav-down');
+    }
+
+    lastScrollTop = scrollTop;
+}
